a86eb2921d24e1e599405e7eaef96ef78e33d642
Võ Hoàng Phúc <contact@vohoangphuc.com>
2025-12-01 16:01:22 +0700

    chore(format): apply biome format

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-"use client";
+'use client';
 
-import { Button } from "@saint-giong/bamboo-ui";
+import { Button } from '@saint-giong/bamboo-ui';
 
 export default function Home() {
-	return (
-		<div className="h-screen w-screen flex items-center justify-center text-center">
-			<Button>Hello, Bamboo UI!</Button>
-		</div>
-	);
+  return (
+    <div className="h-screen w-screen flex items-center justify-center text-center">
+      <Button>Hello, Bamboo UI!</Button>
+    </div>
+  );
 }
