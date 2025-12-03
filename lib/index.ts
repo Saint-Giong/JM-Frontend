e1802cf2658a4ea9e755d03b91c9d0b91d5a1ba3
Võ Hoàng Phúc <contact@vohoangphuc.com>
2025-12-03 22:31:41 +0700

    chore: implement fetcher utils

---
+++ lib/index.ts
@@ -0,0 +1,11 @@
+export { HttpClient, HttpError, http, createHttpClient } from './http';
+export {
+  createQueryFn,
+  createMutationFn,
+  createPostFn,
+  createPutFn,
+  createPatchFn,
+  createDeleteFn,
+  createApiClient,
+  createFetcher,
+} from './fetcher';
