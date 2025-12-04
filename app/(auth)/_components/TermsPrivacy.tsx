7b056f71ab0d5af80f3872dd90592538473b4207
suneruuu <ngao021003@gmail.com>
2025-12-04 23:48:08 +0700

    chore: update Terms and Privacy Policy hrefs

--- app/(auth)/_components/TermsPrivacy.tsx
+++ app/(auth)/_components/TermsPrivacy.tsx
@@ -4,11 +4,11 @@ export function TermsPrivacy() {
     return (
         <p className="text-center text-sm text-muted-foreground mt-6">
             By creating an account, you agree to our{' '}
-            <a href="/terms" className="underline hover:text-foreground">
+            <a href="https://docs.jm.saintgiong.ttr.gg/legal/terms-of-service" target="_blank" className="underline hover:text-foreground">
                 Terms of Service
             </a>{' '}
             and{' '}
-            <a href="/privacy" className="underline hover:text-foreground">
+            <a href="/https://docs.jm.saintgiong.ttr.gg/legal/privacy-policy" target="_blank" className="underline hover:text-foreground">
                 Privacy Policy
             </a>
         </p>
